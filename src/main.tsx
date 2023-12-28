ec10c8a0c3928aecc56b83b9c29952c766592e99
anatonokao <anato.devs@gmail.com>
2023-12-28 13:39:46 +0300

    feat: add more mixins, change variables, create styleguide page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.scss';
+import './styles/global.scss';
+import './styles/codemirror.scss';
+import App from './App.tsx';
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -10,6 +12,7 @@ import {
 } from 'react-router-dom';
 import { setupStore } from './store/store.ts';
 import { Provider } from 'react-redux';
+import DesktopLayout from './components/GraphQl/layouts/DesktopLayout/DesktopLayout.tsx';
 import AuthPage from './components/AuthPage/AuthPage.tsx';
 
 const store = setupStore();
@@ -18,8 +21,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route path="/" element={<App />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/playground" element={<div>GraphiQL Page</div>} />
+      <Route path="/auth" element={<AuthPage/>} />
+      <Route path="/playground" element={<DesktopLayout />} />
     </Route>,
   ),
 );
