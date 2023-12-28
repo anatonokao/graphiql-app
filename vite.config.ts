3d53e2f8e98fae61cfeedcc8dfe2a2a57a5f8b31
anatonokao <anato.devs@gmail.com>
2023-12-28 12:21:32 +0300

    feat: rework styles, create graphql page prototype, create desktop layout

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { configDefaults } from 'vitest/config';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/main.scss";`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
   test: {
     globals: true,
     environment: 'jsdom',
