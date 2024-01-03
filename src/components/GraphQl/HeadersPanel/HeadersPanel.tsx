1fb4d4f53746a34fce4b8961e4d24986159e65d1
anatonokao <anato.devs@gmail.com>
2024-01-03 13:17:37 +0300

    feat: add users headers and vars in request logic

--- src/components/GraphQl/HeadersPanel/HeadersPanel.tsx
+++ src/components/GraphQl/HeadersPanel/HeadersPanel.tsx
@@ -3,8 +3,18 @@ import styles from './HeadersPanel.module.scss';
 import CodeMirror from '@uiw/react-codemirror';
 import { editorTheme } from '@/components/GraphQl/customTheme.ts';
 import { json } from '@codemirror/lang-json';
+import { useAppDispatch, useAppSelector } from '@/store/hooks.ts';
+import { setHeaders } from '@/store/GraphQl/graphqlSlice.ts';
 
 const HeadersPanel: FC = () => {
+  const headers = useAppSelector((state) => state.graphqlSlice.headers);
+
+  const dispatch = useAppDispatch();
+
+  const onChangeHandler = (value: string): void => {
+    dispatch(setHeaders(value));
+  };
+
   return (
     <section className={styles.headersContainer}>
       <CodeMirror
@@ -12,6 +22,8 @@ const HeadersPanel: FC = () => {
         placeholder="JSON format"
         spellCheck={true}
         extensions={[json()]}
+        onChange={onChangeHandler}
+        value={headers}
       />
     </section>
   );
--- src/components/GraphQl/VariablesPanel/VariablesPanel.tsx
+++ src/components/GraphQl/VariablesPanel/VariablesPanel.tsx
@@ -3,14 +3,26 @@ import { json } from '@codemirror/lang-json';
 import { editorTheme } from '@/components/GraphQl/customTheme.ts';
 import CodeMirror from '@uiw/react-codemirror';
 import styles from './VariablesPanel.module.scss';
+import { useAppDispatch, useAppSelector } from '@/store/hooks.ts';
+import { setVars } from '@/store/GraphQl/graphqlSlice.ts';
 const VariablesPanel = () => {
+  const vars = useAppSelector((state) => state.graphqlSlice.vars);
+
+  const dispatch = useAppDispatch();
+
+  const onChangeHandler = (value: string): void => {
+    dispatch(setVars(value));
+  };
+
   return (
     <section className={styles.variablesContainer}>
       <CodeMirror
         theme={editorTheme()}
+        onChange={onChangeHandler}
         placeholder="JSON format"
         spellCheck={true}
         extensions={[json()]}
+        value={vars}
       />
     </section>
   );
