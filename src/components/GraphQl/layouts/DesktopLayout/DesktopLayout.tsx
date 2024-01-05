21eb667659b015e80b1dae9d73781afe98262d85
anatonokao <anato.devs@gmail.com>
2024-01-05 19:39:13 +0300

    feat: add change api url logic,add error if fetching schema failed, add icon for doc btn, add styles for error

--- src/components/GraphQl/layouts/DesktopLayout/DesktopLayout.tsx
+++ src/components/GraphQl/layouts/DesktopLayout/DesktopLayout.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import CodeEditorPanel from '@/components/GraphQl/CodeEditorPanel/CodeEditorPanel.tsx';
 import ResultPanel from '@/components/GraphQl/ResultPanel/ResultPanel.tsx';
 import styles from './DesktopLayout.module.scss';
@@ -11,85 +11,112 @@ import InputUrlApi from '@/components/GraphQl/InputUrlApi/InputUrlApi.tsx';
 import RunBtn from '@/components/GraphQl/RunBtn/RunBtn.tsx';
 import Prettifyer from '@/components/GraphQl/Prettifyer/Prettifyer.tsx';
 import DocPanel from '@/components/GraphQl/DocPanel/DocPanel.tsx';
+import { useAppSelector } from '@/store/hooks.ts';
+import { graphqlAPI } from '@/store/GraphQl/graphqlAPI/graphqlAPI.ts';
 
 const DesktopLayout: FC = () => {
   const [isDocPanelOpen, setIsDocPanelOpen] = useState(false);
-
   const toggleDoc = () => {
     setIsDocPanelOpen((prevState) => !prevState);
   };
+
+  const apiUrl = useAppSelector((state) => state.graphqlSlice.apiUrl);
+
+  const [getSchema, { data, isFetching, isError }] =
+    graphqlAPI.useLazyGetSchemaQuery();
+
+  useEffect(() => {
+    getSchema(apiUrl);
+  }, [apiUrl, getSchema]);
 
   return (
     <>
       <div>Header</div>
       <div className={styles.ide}>
         <InputUrlApi />
-        <PanelGroup direction="horizontal">
-          <button
-            type="button"
-            onClick={toggleDoc}
-            className={styles.docToggler}
-          ></button>
-          <Panel
-            minSize={10}
-            maxSize={9999}
-            defaultSize={30}
-            className={styles.docPanel}
-            style={{ display: isDocPanelOpen ? 'flex' : 'none' }}
-          >
-            <DocPanel />
-          </Panel>
-          <PanelResizeHandle
-            className={styles.separator}
-            style={{ display: isDocPanelOpen ? 'flex' : 'none' }}
-          />
-          <Panel minSize={30} maxSize={9999} className={styles.panel}>
-            <PanelGroup direction="vertical">
-              <Panel className={styles.panel}>
-                <div className={styles.header}>
-                  <h5 className={styles.panelTitle}>Operation</h5>
-                  <div className={styles.headerRight}>
-                    <Prettifyer />
-                    <RunBtn />
-                  </div>
-                </div>
-                <CodeEditorPanel />
-              </Panel>
-              <PanelResizeHandle className={styles.horizontalSeparator} />
-              <Panel className={styles.panel}>
-                <Tabs className={styles.tabsWrapper}>
-                  <TabList className={styles.tabsNav}>
-                    <Tab
-                      className={styles.tabBtn}
-                      selectedClassName={styles.tabBtnActive}
-                    >
-                      Vars
-                    </Tab>
-                    <Tab
-                      className={styles.tabBtn}
-                      selectedClassName={styles.tabBtnActive}
-                    >
-                      Headers
-                    </Tab>
-                  </TabList>
-                  <TabPanel selectedClassName={styles.tab}>
-                    <VariablesPanel />
-                  </TabPanel>
-                  <TabPanel selectedClassName={styles.tab}>
-                    <HeadersPanel />
-                  </TabPanel>
-                </Tabs>
-              </Panel>
-            </PanelGroup>
-          </Panel>
-          <PanelResizeHandle className={styles.separator} />
-          <Panel minSize={30} maxSize={9999} className={styles.panel}>
-            <div className={styles.header}>
-              <h5 className={styles.panelTitle}>Results</h5>
+        {isError ? (
+          <div className={styles.error}>
+            <div className={styles.errorTitle}>
+              Oops, seems something went wrong!
             </div>
-            <ResultPanel />
-          </Panel>
-        </PanelGroup>
+            <div className={styles.errorText}>
+              Maybe our coders forgot to eat, and now they are not performing at
+              their best. We are already feeding them cookies 🍪, but just to be
+              sure, please check that the server link is correct, the server is
+              work, and it supports GraphQL queries.
+            </div>
+          </div>
+        ) : (
+          <PanelGroup direction="horizontal">
+            <button
+              type="button"
+              onClick={toggleDoc}
+              className={styles.docToggler}
+              title="Docs"
+            >
+              <img src="src/assets/doc-btn.svg" alt="docs" />
+            </button>
+            <Panel
+              minSize={10}
+              maxSize={9999}
+              defaultSize={30}
+              className={styles.docPanel}
+              style={{ display: isDocPanelOpen ? 'flex' : 'none' }}
+            >
+              <DocPanel schema={data} />
+            </Panel>
+            <PanelResizeHandle
+              className={styles.separator}
+              style={{ display: isDocPanelOpen ? 'flex' : 'none' }}
+            />
+            <Panel minSize={30} maxSize={9999} className={styles.panel}>
+              <PanelGroup direction="vertical">
+                <Panel className={styles.panel}>
+                  <div className={styles.header}>
+                    <h5 className={styles.panelTitle}>Operation</h5>
+                    <div className={styles.headerRight}>
+                      <Prettifyer />
+                      <RunBtn />
+                    </div>
+                  </div>
+                  <CodeEditorPanel isLoading={isFetching} schema={data} />
+                </Panel>
+                <PanelResizeHandle className={styles.horizontalSeparator} />
+                <Panel className={styles.panel}>
+                  <Tabs className={styles.tabsWrapper}>
+                    <TabList className={styles.tabsNav}>
+                      <Tab
+                        className={styles.tabBtn}
+                        selectedClassName={styles.tabBtnActive}
+                      >
+                        Vars
+                      </Tab>
+                      <Tab
+                        className={styles.tabBtn}
+                        selectedClassName={styles.tabBtnActive}
+                      >
+                        Headers
+                      </Tab>
+                    </TabList>
+                    <TabPanel selectedClassName={styles.tab}>
+                      <VariablesPanel />
+                    </TabPanel>
+                    <TabPanel selectedClassName={styles.tab}>
+                      <HeadersPanel />
+                    </TabPanel>
+                  </Tabs>
+                </Panel>
+              </PanelGroup>
+            </Panel>
+            <PanelResizeHandle className={styles.separator} />
+            <Panel minSize={30} maxSize={9999} className={styles.panel}>
+              <div className={styles.header}>
+                <h5 className={styles.panelTitle}>Results</h5>
+              </div>
+              <ResultPanel />
+            </Panel>
+          </PanelGroup>
+        )}
       </div>
     </>
   );
