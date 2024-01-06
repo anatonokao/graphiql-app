4c7e740120e324e3bf4eff6671ae3859c3aed784
Darya <watashi.no.kao24@gmail.com>
2024-01-07 02:40:59 +0300

    feat: add basic tests for auth page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,15 +3,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App.tsx';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from '@/store/store.ts';
 
 describe('app', () => {
   test('app render', async () => {
     render(
       <BrowserRouter>
-        <App />
+        <Provider store={setupStore()}>
+          <App />
+        </Provider>
       </BrowserRouter>,
     );
 
-    expect(await screen.findByText('ДО ДЕДЛАЙНА')).toBeInTheDocument();
+    expect(await screen.findByText('Home')).toBeInTheDocument();
   });
 });
